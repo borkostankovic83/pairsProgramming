73a56e3aeaa581549af406636248bbe894ec5e93
Ryan Marinoff <ryan.marinoff@outlook.com>
2020-03-05 11:12:37 -0500

    initial setup

---
+++ aggregatedService/src/main/java/com/revature/cognizant/aggregatedService/AggregatedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.cognizant.aggregatedService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatedServiceApplication.class, args);
+	}
+
+}
---
+++ aggregatedService/src/test/java/com/revature/cognizant/aggregatedService/AggregatedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.cognizant.aggregatedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
